a1436131874a567f2e477cad277a3e635d9c0224
NandhiniNavaneethan251 <69723797+NandhiniNavaneethan251@users.noreply.github.com>
2021-02-17 09:47:54 +0530

    Changes from default template

---
+++ src/app/condition/condition.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-condition',
+  templateUrl: './condition.component.html',
+  styleUrls: ['./condition.component.css']
+})
+export class ConditionComponent implements OnInit {
+displayName=true;
+displayName1=false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
